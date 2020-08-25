c27ffcc8fc38acc5f213e767f61d65fe653bb408
Shivang Medhekar <shivangmedhekar@gmail.com>
2020-08-25 10:12:12 +0530

    Added Code

---
+++ ecommerce_project/store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('product/', views.product_page, name='product'),
+]
+
---
+++ ecommerce_project/store/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def product_page(request):
+    return render(request, 'product.html')
